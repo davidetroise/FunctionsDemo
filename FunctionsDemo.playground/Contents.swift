f7d255d46c6fe56580c7a9ae8684f114a18785b5
Davide Troise <davide@troise.net>
2023-01-02 01:32:46 +0100

    Complete code

--- FunctionsDemo.playground/Contents.swift
+++ FunctionsDemo.playground/Contents.swift
@@ -1,3 +1,34 @@
-import UIKit
+func square(number: Int) -> Int {
+    number * number
+}
 
-var greeting = "Hello, playground"
+let n = 5
+let result = square(number: n)
+
+func greet(from name: String, to recipient: String) {
+    print("Hello \(recipient)! I am \(name)")
+}
+
+greet(from: "Mario", to: "Luigi")
+
+func average(numbers: Double...) -> Double {
+    var sum = 0.0
+    for number in numbers {
+        sum += number
+    }
+    return sum / Double(numbers.count)
+}
+
+average(numbers: 2.1, 2.7, 3.2, 7.0)
+
+func reverse(a: inout Int, b: inout Int) {
+    let temp = a
+    a = b
+    b = temp
+}
+
+var x = 5
+var y = 10
+reverse(a: &x, b: &y)
+print(x)
+print(y)
